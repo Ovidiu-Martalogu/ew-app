340a5ff6e1ffaa67b755ebe131da1afd1635fa40
Ovidiu <eu_oni@yahoo.com>
2026-04-18 18:32:59 +0300

    useEffect for FirstPage

--- src/features/First-page/FirstPage.tsx
+++ src/features/First-page/FirstPage.tsx
@@ -1,46 +1,49 @@
 
+import { useEffect, useState } from "react";
 import styles from "./firstpage.module.css";
 
-export function FirstPage() {
-    return (
-        <div className={styles.container}>
-              <section className={styles.upAndDownText}>
-                <h2 className={styles.title}>
-                    Monitorizează-ți cheltuielile ușor și eficient
-                </h2>
-                <p className={styles.subtitle}>
-                    Ține-ți finanțele sub control, creează bugete și vezi unde se duc banii tăi, toate într-o singură aplicație modernă.
-                </p>
-              
-            </section>
 
-            <section className={styles.sectionCards}>
-                <h3 className={styles.sectionTitle}>Funcționalități</h3>
-                <div className={styles.grid}>
-                    <div className={styles.card}>
-                        <h4>📊 Monitorizare în timp real</h4>
-                        <p>Adaugă cheltuieli și urmărește-le instant.</p>
-                    </div>
-                    <div className={styles.card}>
-                        <h4>💰 Bugete personalizate</h4>
-                        <p>Setează limite pentru fiecare categorie de cheltuieli.</p>
-                    </div>
-                    <div className={styles.card}>
-                        <h4>📈 Rapoarte detaliate</h4>
-                        <p>Analizează-ți obiceiurile financiare cu grafice clare.</p>
-                    </div>
-                </div>
-            </section>
+const apiUrl = `${import.meta.env.VITE_API_URL}/firstpage`;
 
-                  <section className={styles.upAndDownText}>
-                <h3>Preia controlul asupra banilor tăi</h3>
-                <p>Începe chiar azi și vezi diferența.</p>
-                
-            </section>
-
-            
-        </div>
-    );
+type Card = {
+    title: string;
+    subtitle: string;
 };
 
-;
+export function FirstPage() {
+
+    const [firstPage, setFirstPage] = useState<Card[]>([]);
+
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Request failed");
+                }
+                return response.json();
+            })
+            .then(data => setFirstPage(data))
+            .catch(error => console.error(error));
+    }, []);
+
+    console.log(firstPage);
+
+
+
+    if (!firstPage.length) return <p>Loading...</p>;
+
+
+    return (
+        <div className={styles.container}>
+            {firstPage.map((card, i) => (
+                <div key={i}>
+                    <h3>{card.card1.cardTitle}</h3>
+                    <p>{card.subtitle}</p>
+                </div>
+            ))}
+        </div>
+    );
+
+}
+
