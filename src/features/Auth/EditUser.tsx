8c0d96a596dc81904cbb38999eb2acabd364ec25
Ovidiu <eu_oni@yahoo.com>
2026-04-19 23:42:51 +0300

    EditUser form

--- src/features/Auth/EditUser.tsx
+++ src/features/Auth/EditUser.tsx
@@ -1,69 +1,141 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { FirstPage } from "../First-page/FirstPage";
 
+import styles from "../../features/Auth/context/EditUser.module.css"
 
+const apiUrl = `${import.meta.env.VITE_API_URL}/users`;
 
+const apiUrlLogin = `${import.meta.env.VITE_API_URL}/login`;
+
+type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+};
 
 export function EditUser() {
+    const [user, setUser] = useState<User | null>(null);
+    
+    
+    const userId = localStorage.getItem("auth");
+    
+    
+    useEffect(() => {
+                
+        fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(`Id este: ${data.user}`);
+            setUser(data.user);
+        });
+        console.log(userId);
+    }, []);
 
 
+
+    async function updateUser(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const form = new FormData(e.currentTarget);
+
+        const firstName = form.get("firstName");
+        const lastName = form.get("lastName");
+        const email = form.get("email");
+        const password = form.get("password");
+
+
+        await fetch(`${apiUrl}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ firstName, lastName, email, password }),
+        });
+
+
+    }
     return (
-        <>
+       <>
+
 
             <h2>Edit your data</h2>
-            <form className="brandForm" >
+             {user && (
+               
+                        <form onSubmit={updateUser} className={styles.brandForm} >
+                 
+                        <div className={styles.formGroup}>
 
+                            <label htmlFor="firstName">Change First Name</label>
 
-                <label htmlFor="firstName">Change First Name</label>
-                <input
-                    type="text"
-                    id="firstName"
-                    name="firstName"
+                            <input
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                placeholder={`Your first name is${user.firstName}`}
+                                defaultValue={user.firstName}
 
-                    onChange={EditUser}
-                />
+                            />
+                        </div>
+                        <div className={styles.formGroup}>
 
+                            <label htmlFor="lastName">Change Last Name</label>
 
-                <label htmlFor="lastName">Change Last Name</label>
-                <input
-                    type="text"
-                    id="lastName"
-                    name="lastName"
+                            <input
+                                type="text"
+                                id="lastName"
+                                name="lastName"
+                                placeholder={`Your first name is${user.lastName}`}
+                                defaultValue={user.lastName}
 
-                    onChange={EditUser}
-                />
+                            />
+                        </div>
+                        <div className={styles.formGroup}>
 
-                <label htmlFor="email">Change Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
+                            <label htmlFor="email">Change Email</label>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                 placeholder={`Your first name is${user.email}`}
+                                defaultValue={user.email}
 
-                    onChange={EditUser}
-                />
+                            />
+                        </div>
+                        <div className={styles.formGroup}>
 
+                            <label htmlFor="password">Change Password</label>
 
-                <label htmlFor="password">Change Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Set your new password"
 
-                    onChange={EditUser}
-                />
+                            />
+                        </div>
+                        <div className={styles.formGroup}>
 
-                <label htmlFor="retypePassword">Retype Password</label>
-                <input
-                    type="password"
-                    id="retypePassword"
-                    name="retypePassword"
+                            <label htmlFor="retypepassword">Retype your new Password</label>
 
-                    onChange={EditUser}
-                />
+                            <input
+                                type="password"
+                                id="retypepassword"
+                                name="retypepassword"
+                                placeholder="Retype your new Password"
 
-
+                            />
+                        </div>
+                                
 
                 <button type="submit">Change</button>
             </form>
-
-        </>
-    )
+             )}
+             <Link to="/" onClick={FirstPage}>
+                <strong >Cancel</strong>
+            </Link>
+            
+             
+            </>
+        )   
 }
