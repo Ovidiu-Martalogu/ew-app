2b215d8d13d5c0810c1680900c8f161f7d5f490a
Ovidiu <eu_oni@yahoo.com>
2026-04-29 23:52:54 +0300

    fetch db and add status on db

--- src/features/Income/EditOneIncome.tsx
+++ src/features/Income/EditOneIncome.tsx
@@ -1,12 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router";
 
 
 
 const apiUrl = `${import.meta.env.VITE_API_URL}/income`;
 
-export function EditOneIncome() {
+type passive = {
+    source: string;
+    amount: number
+}
 
+type oneIncome = {
+    amount: number;
+    category: string;
+    status: string;
+    deleted: boolean;
+    id: number;
+    date: Date;
+    userId: number;
+    passiveIncome: passive
+
+}
+
+
+export function EditOneIncome() {
+    const [oneIncome, setOneIncome] = useState<oneIncome[]>([])
     useEffect(() => {
         fetch(apiUrl)
             .then(response => {
@@ -15,19 +33,44 @@ export function EditOneIncome() {
                 }
                 return response.json();
             })
+            .then((data) => setOneIncome(data))
             .catch(error => console.error(error))
-            .then((data) => console.log(data));
+
+
 
     }, []);
     return (
         <>
             <h1>Edit</h1>
 
+            {oneIncome.map((key, i) => (
+                <div key={i}>
+                    <p>Category: {key.category}</p>
+                    <p>Status:{key.status}</p>
+                    <p>Date:{key.date}</p>
+                    <p>Amount:{key.amount}</p>
 
-            <NavLink to="/income">
+                </div>
+            ))}
+            {oneIncome && (
+                <form>
+                    <div >
+                        <label htmlFor="status">Status</label>
+                        <input
+                            type="text"
+                            id="status"
+                            name="status"
+                            defaultValue={oneIncome[0].status}
+                        />
+                    </div>
+                    <button type="submit">Update</button>
+                    <NavLink to="/income">
 
-                <button>Back</button>
-            </NavLink>
+                        <button>Back</button>
+                    </NavLink>
+                </form>
+            )}
+
         </>
     )
 }
