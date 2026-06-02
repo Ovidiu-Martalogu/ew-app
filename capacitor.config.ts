262eb816af2670c44c0808da545b8b5ce72ba622
Ovidiu <eu_oni@yahoo.com>
2026-06-02 23:49:44 +0300

    adding capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ew.app',
+  appName: 'ew-app',
+  webDir: 'build'
+};
+
+export default config;
